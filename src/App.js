78fe3211df6e4550b9c638116dc9e18dc704bab6
dibdas <dibyandu08das@gmail.com>
2023-08-28 17:31:30 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import Login from './pages/login/Login';
+import SignUp from './pages/signUp/SignUp';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
