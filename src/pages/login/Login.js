c0c16d13d16bed87c41fdef1d8e159c0d9c3c7ed
dibdas <dibyandu08das@gmail.com>
2023-08-28 17:33:12 +0530

    modified

---
+++ src/pages/login/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import axiosClient from "../../utils/axiosClient";
+import {
+  KEY_ACCESS_TOKEN,
+  settingItemFromLocalStorage,
+  setItem,
+} from "../../utils/localStorageManage";
+import "./Login.scss";
+
+function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  // console.log(axiosClient.baseUrl);
+  async function handleSubmit(event) {
+    event.preventDefault(); // it prevent to reload the page
+    try {
+    //   const result = await axiosClient.post("/auth/login", {
+    //     email,
+    //     password,
+    //   });
+
+      // console.log(result);
+      // settingItemFromLocalStorage(
+      // setItem(KEY_ACCESS_TOKEN, JSON.stringify(result.accessToken));
+      // setItem(KEY_ACCESS_TOKEN, result.accessToken);
+      // console.log(result.accessToken);
+      console.log("logged in ");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="login">
+      <div className="login-box">
+        <h2 className="heading">Login</h2>
+        <form onSubmit={handleSubmit}>
+          {/* providing id to the input field is important,as id can be linked to label */}
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="email"
+            id="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="password"
+            id="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit" onSubmit={handleSubmit}>
+            Login
+          </button>
+        </form>
+        <p className="signUp">
+          Need an account? <Link to="/signup">SignUp</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/signUp/SignUp.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import axiosClient from "../../utils/axiosClient";
+import "./Signup.scss";
+
+function SignUp() {
+  const [email, setEmail] = useState();
+  const [password, setpassword] = useState();
+  const [name, setName] = useState();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      // const result = await axiosClient.post("/auth/signup", {
+      //   name,
+      //   email,
+      //   password,
+      // });
+      // console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="signup">
+      <div className="signup-box">
+        <h2 className="heading">SgnUp</h2>
+
+        <form action="" onSubmit={handleSubmit}>
+          {/* providing id to the input field is important,as id can be linked to label */}
+
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="name"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="password"
+            id="password"
+            onChange={(e) => setpassword(e.target.value)}
+          />
+
+          <button type="submit" onSubmit={handleSubmit}>
+            Sign Up
+          </button>
+        </form>
+        <p className="signUp">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
