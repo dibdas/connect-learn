5e9fd87a21a5b1514cbeb10b7843d8eb62a2d2fd
dibdas <dibyandu08das@gmail.com>
2023-08-29 16:39:10 +0530

    modified

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -20,48 +20,49 @@ function SignUp() {
       console.log(error);
     }
   }
-  return (
-    <div className="signup">
-      <div className="signup-box">
-        <h2 className="heading">SgnUp</h2>
 
-        <form action="" onSubmit={handleSubmit}>
-          {/* providing id to the input field is important,as id can be linked to label */}
+  // return (
+  //   <div className="signup">
+  //     <div className="signup-box">
+  //       <h2 className="heading">SgnUp</h2>
 
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="name"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-          />
+  //       <form action="" onSubmit={handleSubmit}>
+  //         {/* providing id to the input field is important,as id can be linked to label */}
 
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="email"
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+  //         <label htmlFor="name">Name</label>
+  //         <input
+  //           type="text"
+  //           className="name"
+  //           id="name"
+  //           onChange={(e) => setName(e.target.value)}
+  //         />
 
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="password"
-            id="password"
-            onChange={(e) => setpassword(e.target.value)}
-          />
+  //         <label htmlFor="email">Email</label>
+  //         <input
+  //           type="email"
+  //           className="email"
+  //           id="email"
+  //           onChange={(e) => setEmail(e.target.value)}
+  //         />
 
-          <button type="submit" onSubmit={handleSubmit}>
-            Sign Up
-          </button>
-        </form>
-        <p className="signUp">
-          Already have an account? <Link to="/login">Login</Link>
-        </p>
-      </div>
-    </div>
-  );
+  //         <label htmlFor="password">Password</label>
+  //         <input
+  //           type="password"
+  //           className="password"
+  //           id="password"
+  //           onChange={(e) => setpassword(e.target.value)}
+  //         />
+
+  //         <button type="submit" onSubmit={handleSubmit}>
+  //           Sign Up
+  //         </button>
+  //       </form>
+  //       <p className="signUp">
+  //         Already have an account? <Link to="/login">Login</Link>
+  //       </p>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default SignUp;
