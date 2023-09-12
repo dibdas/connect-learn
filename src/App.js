841a178df3c1594104c9fe0035ad3ac3cf016ce3
dibdas <dibyandu08das@gmail.com>
2023-09-12 23:53:20 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signUp/SignUp";
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
+import Home from "./pages/home/Home";
 import Notifications from "./components/notifications/Notifications";
 import Dashboard from "./pages/dashboard/Dashboard";
+import CourseDetails from "./pages/courseDetails/CourseDetails";
+import CourseList from "./pages/courseList/CourseList";
+import Quiz from "./pages/quiz/Quiz";
+import FormUpload from "./pages/upload/FormUpload";
+import NewFormUpload from "./pages/upload/NewFormUpload";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="App">
-      <Notifications />
+      {/* <Notifications /> */}
       <Routes>
         {" "}
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/signup" element={<SignUp />} />
-        
-         */}
         <Route path="/" element={<Home />} />
+        <Route path="/courselist" element={<CourseList />} />
+        <Route path="/courses/:name" element={<CourseDetails />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/uploadquiz" element={<FormUpload />} />
+        {/* <Route path="/uploadquiz" element={<NewFormUpload />} /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
